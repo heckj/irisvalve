dc76499d44ff05a3e3b01ae281641b213033e7ae
joe <joe@60eaf18d-bf02-0410-b178-357d2da7dd85>
2009-10-06 04:12:27 +0000

    updating model and adding a few doctests

--- irisvalve/models.py
+++ irisvalve/models.py
@@ -8,6 +8,8 @@ class Tile(db.Model):
     """Basic user profile with personal details."""
     name = db.StringProperty(required=True)
     image = db.LinkProperty(required=True)
+    blocked = db.BooleanProperty(required=True)
+    opacity = db.IntegerProperty(required=True)
     
     def __unicode__(self):
         return '%s %s' % (self.name, self.image)
@@ -19,12 +21,16 @@ class Tile(db.Model):
     def get_absolute_url(self):
         return ('irisvalve.views.show_tile', (), {'key': self.key()})
 
-class MapLocation(db.Model):
+class Map(db.Model):
+    """
+    >>> open = Tile(image='http://www.foo.com/open.png',name='open',blocked=False,opacity=0).put()
+    >>> Map(x=1,y=1,name="testmap",tile=open).put()
+    datastore_types.Key.from_path(u'irisvalve_map', 8L, _app_id_namespace=u'irisvalve')
+    """
     x = db.IntegerProperty(required=True)
     y = db.IntegerProperty(required=True)
     tile = db.ReferenceProperty(Tile, required=True)
-    #name = db.StringProperty(required=True)
-    #file = db.BlobProperty(required=True)
+    name = db.StringProperty(required=True)
 
     @permalink
     def get_absolute_url(self):
@@ -32,8 +38,23 @@ class MapLocation(db.Model):
     def __unicode__(self):
         return u'MapLocation [%s,%s]: %s' % (self.x, self.y, self.tile.name)
 
-# class Contract(db.Model):
-#     employer = db.ReferenceProperty(Person, required=True, collection_name='employee_contract_set')
-#     employee = db.ReferenceProperty(Person, required=True, collection_name='employer_contract_set')
-#     start_date = db.DateTimeProperty()
-#     end_date = db.DateTimeProperty()
+class GameObject(db.Model):
+    """
+    >>> GameObject.all().count()
+    0
+    >>> x = GameObject().put()
+    >>> GameObject.all().count()
+    1
+    >>> x=GameObject.all()[0]
+    >>> x.weight
+    0.0
+    >>> x.image
+    >>> x.active
+    False
+    >>> x.delete()
+    >>> GameObject.all().count()
+    0
+    """
+    image = db.LinkProperty(required=False)
+    active = db.BooleanProperty(required=True,default=False)
+    weight = db.FloatProperty(required=True,default=0.0)
--- irisvalve/tests/modeltests.py
+++ irisvalve/tests/modeltests.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.test import TestCase
 from google.appengine.ext import db
-from irisvalve.models import Tile, MapLocation
+from irisvalve.models import Tile, Map, GameObject
 
 class TileTest(TestCase):
 	def test_nothing(self):
 		self.assertTrue(1)
 	def setUp(self):
-		Tile(image='http://www.foo.com/open.png',name='open').put()
-		Tile(image='http://www.foo.com/blocked.png',name='blocked').put()
+		Tile(image='http://www.foo.com/open.png',name='open',blocked=False,opacity=0).put()
+		Tile(image='http://www.foo.com/blocked.png',name='blocked',blocked=True,opacity=10).put()
 	def test_setup(self):
 		self.assertEquals(2,len(Tile.all()))
 		self.assertEquals(1,len(Tile.all().filter('name =','open')))
 		self.assertEquals(1,len(Tile.all().filter('name =','blocked')))
 		open = Tile.all().filter('name =','open')[0]
-		MapLocation(tile=open,x=0,y=0).put()
-		self.assertEquals(1,len(MapLocation.all()))
+		Map(name="testmap",tile=open,x=0,y=0).put()
+		self.assertEquals(1,len(Map.all()))
 
 class BasicTileTest(TestCase):
 	def test_basic_create(self):
 		#clear all tiles
 		for obj in Tile.all():
 			x.delete()
-		x = Tile(image='http://www.foo.com/',name='foo')
+		x = Tile(image='http://www.foo.com/',name='foo',blocked=False,opacity=5)
 		x.put()
 		self.assertEquals(1,len(Tile.all()))
 		x.delete()
@@ -30,12 +30,12 @@ class BasicTileTest(TestCase):
 
 class MapTileTest(TestCase):
 	def setUp(self):
-		open = Tile(image='http://www.foo.com/open.png',name='open')
+		open = Tile(image='http://www.foo.com/open.png',name='open',blocked=False,opacity=0)
 		open.put()
-		blocked = Tile(image='http://www.foo.com/blocked.png',name='blocked')
+		blocked = Tile(image='http://www.foo.com/blocked.png',name='blocked',blocked=True,opacity=10)
 		blocked.put()
 		for x in range(10):
 			for y in range(10):
-				MapLocation(tile=open,x=x,y=y).put()
+				Map(name="testmap",tile=open,x=x,y=y).put()
 	def test_setup(self):
-		self.assertEquals(100,len(MapLocation.all()))
+		self.assertEquals(100,len(Map.all()))
